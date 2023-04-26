5e4e1196cb49790a573ffd434405c0de56b87da7
Max <maxhuffy@gmail.com>
2023-04-25 17:01:39 -0700

    fixed svg generation
    SVGs are now in the correct orientation, and less clipping issues.

--- src/font-to-3D.py
+++ src/font-to-3D.py
@@ -5,6 +5,8 @@ from fontTools.pens.svgPathPen import SVGPathPen
 from fontTools.pens.recordingPen import RecordingPen
 from fontTools.pens.transformPen import TransformPen
 
+from svgpathtools import parse_path
+
 def otf_to_svg(input_folder, output_folder):
     for input_otf in glob.glob(os.path.join(input_folder, "*.otf")):
         font = TTFont(input_otf)
@@ -25,9 +27,20 @@ def otf_to_svg(input_folder, output_folder):
             transform_pen = TransformPen(pen, (1, 0, 0, -1, 0, font["head"].unitsPerEm))
             glyph.draw(transform_pen)
 
+            svg_commands = pen.getCommands()
+
+            # Calculate the bounding box using svgpathtools.parse_path
+            path = parse_path(svg_commands)
+            
+            if len(path) > 0:
+                bounds = path.bbox()
+            else:
+                bounds = (0, 0, 0, 0)
+
             with open(os.path.join(font_output_folder, f"{glyph_name}.svg"), "w") as svg_file:
-                svg_file.write(f'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {glyph.width} {font["head"].unitsPerEm}">\n')
-                svg_file.write(f'<path d="{pen.getCommands()}"/>\n')
+                # Set the viewBox attribute to fit the bounding box
+                svg_file.write(f'<svg xmlns="http://www.w3.org/2000/svg" viewBox="{bounds[0]} {bounds[1]} {bounds[2] - bounds[0]} {bounds[3] - bounds[1]}">\n')
+                svg_file.write(f'<path d="{svg_commands}"/>\n')
                 svg_file.write("</svg>")
 
 # Set input folder and output folder for SVG files
